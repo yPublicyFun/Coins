095e3e1677e5199fb7ca653570c31b33326a01d1
yPublicyFun <41261900+yPublicyFun@users.noreply.github.com>
2018-07-22 14:07:04 +0200

    Create Coins.php

---
+++ src/Dushy/Coins.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Dushy;
+
+//Basis
+use pocketmine\plugin\PluginBase;
+
+//Für Münzen
+use pocketmine\event\Listeners;
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Coins extends PluginBase implements Listeners {
+	
+	public $prefix = "§7[§eMünzen§7]";
+	
+	public function onEnable() {
+	$this->getServer()->getLogger()->info("§bPlugin §eMünzen §bwurde aktiviert!");
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool{ 
+        if($cmd->getName() == "coins"){
+            switch($args[0]){
+            	case "add":
+                  if(!empty($args[1]) && !empty($args[2])){
+                        API::addCoins($args[1], intval($args[2]));
+                        $sender->sendMessage("§a".$player." hat nun ".$args[2]." Münzen erhalten!");
+                    }else{
+                        $sender->sendMessage("§cBenutzung: /coins add <player> <coins>");
+                    }
+                    break;
+                case "set":
+                    if(!empty($args[1]) && !empty($args[2])){
+                        API::setCoins($args[1], intval($args[2]));
+                        $sender->sendMessage("§a".$player." wurden nun ".$args[2]." Münzen gesetzt!");
+                    }else{
+                        $sender->sendMessage("§cBenutzung: /coins set <player> <coins>");
+                        }
+                    break;
+                case "remove":
+                    if(!empty($args[1]) && !empty($args[2])){
+                        API::removeCoins($args[1], intval($args[2]));
+                        $sender->sendMessage("§a".$player." wurden ".$args[2]." Münzen entfernt");
+                    }else{
+                        $sender->sendMessage("§cBenutzung: /coins remove <player> <coins>");
+                        }
+                        break;
+                    
+            }
+            return true;
+        }
+     }
+  }
+  
+  class API implements Listeners {
+    
+    public static function addPlayer(string $name){
+        
+    }
+    
+    public static function getCoins(string $player): int{
+        
+    }
+    
+    public static function setCoins(string $player, int $coins){
+        
+    }
+    
+    public static function addCoins(string $player, int $coins){
+        
+    }
+    
+    public static function removeCoins(string $player, int $coins){
+        
+     }    
+  }
+}
